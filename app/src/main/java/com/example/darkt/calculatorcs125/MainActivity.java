1f5424b7dc59a67d44eb678d293d1f1965ac2f70
Earl B Chae <earlbc2@illinois.edu>
2018-12-04 23:01:54 -0600

    2nd done

--- app/src/main/java/com/example/darkt/calculatorcs125/MainActivity.java
+++ app/src/main/java/com/example/darkt/calculatorcs125/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.darkt.calculatorcs125;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
@@ -15,6 +16,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button graphBtn = (Button) findViewById(R.id.graphButton);
+        graphBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), Graph.class);
+                startActivity(startIntent);
+            }
+        });
     }
     public void buttonOnClick(View v) {
         Button calculate = (Button) v;
