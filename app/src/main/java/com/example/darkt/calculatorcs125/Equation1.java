30b3505e31eb6f5b33808470d9e8eacfa1cb9f2b
Joshua <jshin88@illinois.edu>
2018-12-05 00:58:08 -0600

    Commit 1

---
+++ app/src/main/java/com/example/darkt/calculatorcs125/Equation1.java
@@ -0,0 +1,19 @@
+package com.example.darkt.calculatorcs125;
+
+public class Equation1 {
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_graph);
+//
+//        Button mainBtn = (Button) findViewById(R.id.MainButton);
+//        mainBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent startIntent = new Intent(getApplicationContext(), MainActivity.class);
+//                startActivity(startIntent);
+//            }
+//        });
+//    }
+}
+
