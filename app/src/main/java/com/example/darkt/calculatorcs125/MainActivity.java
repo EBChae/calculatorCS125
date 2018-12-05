5a6ff559a36e163e91e5b14f37685831e322a338
Earl B Chae <earlbc2@illinois.edu>
2018-12-04 22:43:26 -0600

    first UI done

--- app/src/main/java/com/example/darkt/calculatorcs125/MainActivity.java
+++ app/src/main/java/com/example/darkt/calculatorcs125/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.darkt.calculatorcs125;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void buttonOnClick(View v) {
+        Button calculate = (Button) v;
+        EditText input = (EditText)findViewById(R.id.inputBox1);
+        String calculated = gForce(input.getText().toString());
+        TextView output = (TextView) findViewById(R.id.outputBox1);
+        output.setText(calculated);
+
+        Toast toast = new Toast(getApplicationContext());
+        toast.setGravity(Gravity.TOP | Gravity.LEFT, 0 , 0);
+        toast.makeText(MainActivity.this, "calculated", toast.LENGTH_SHORT).show();
+    }
+    public String gForce(String mass) {
+        double convMass = Double.parseDouble(mass);
+        double result = convMass * 9.8;
+        return String.format("%.3f", result);
     }
 }
