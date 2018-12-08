ba9897e6d2647e1a4c330dc585a8603702e1bb5b
Earl B Chae <earlbc2@illinois.edu>
2018-12-08 17:24:54 -0600

    api partial

--- app/src/main/java/com/example/darkt/calculatorcs125/Equation.java
+++ app/src/main/java/com/example/darkt/calculatorcs125/Equation.java
@@ -3,8 +3,12 @@ package com.example.darkt.calculatorcs125;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Equation extends AppCompatActivity {
 
--- app/src/main/java/com/example/darkt/calculatorcs125/Graph.java
+++ app/src/main/java/com/example/darkt/calculatorcs125/Graph.java
@@ -6,13 +6,30 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 public class Graph extends AppCompatActivity {
+
+    LineGraphSeries<DataPoint> series;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph);
 
+        double y,x;
+        x = -5.0;
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        series = new LineGraphSeries<DataPoint>();
+        for (int i = 0; i < 500; i++) {
+            x += 0.1;
+            y = Math.sin(x);
+            series.appendData(new DataPoint(x, y), true, 500);
+        }
+        graph.addSeries(series);
         Button mainBtn = (Button) findViewById(R.id.MainButton);
         mainBtn.setOnClickListener(new View.OnClickListener() {
             @Override
