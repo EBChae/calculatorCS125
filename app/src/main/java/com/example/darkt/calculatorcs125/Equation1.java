45d106c87fa114de8d74d0392eef114b937e2932
EBChae <gkwkemvos1>
2018-12-08 19:39:32 -0600

    eqn1 done

--- app/src/main/java/com/example/darkt/calculatorcs125/Equation1.java
+++ app/src/main/java/com/example/darkt/calculatorcs125/Equation1.java
@@ -3,8 +3,12 @@ package com.example.darkt.calculatorcs125;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Equation1 extends AppCompatActivity {
 
@@ -21,5 +25,52 @@ public class Equation1 extends AppCompatActivity {
                 startActivity(startIntent);
             }
         });
+    }
+    public void eqnCalc1Btn(View v) {
+        String initialVelocity = ((EditText)findViewById(R.id.getvInitial)).getText().toString();
+        String finalVelocity = ((EditText)findViewById(R.id.getvFinal)).getText().toString();
+        String time = ((EditText)findViewById(R.id.getTime)).getText().toString();
+        String displacement = ((EditText)findViewById(R.id.getX)).getText().toString();
+
+        Button calculate = (Button) v;
+        String calculated = kinematics(initialVelocity, finalVelocity, time, displacement);
+        TextView output = (TextView) findViewById(R.id.answerFound1);
+        output.setText(calculated);
+
+        Toast toast = new Toast(getApplicationContext());
+        toast.setGravity(Gravity.TOP | Gravity.LEFT, 0 , 0);
+        toast.makeText(Equation1.this, "calculated", toast.LENGTH_SHORT).show();
+    }
+    public String kinematics(String vi, String vf, String t, String d) {
+        if (vf.equals("")) {
+            double convVI = Double.parseDouble(vi);
+            double convD = Double.parseDouble(d);
+            double convT = Double.parseDouble(t);
+            double convVF = 2 * convD / convT - convVI;
+            return String.format("%.3f", convVF);
+
+        } else if (vi.equals("")) {
+            double convVF = Double.parseDouble(vf);
+            double convD = Double.parseDouble(d);
+            double convT = Double.parseDouble(t);
+            double convVI = 2 * convD / convT - convVF;
+            return String.format("%.3f", convVI);
+
+        } else if (d.equals("")) {
+            double convVF = Double.parseDouble(vf);
+            double convVI = Double.parseDouble(vi);
+            double convT = Double.parseDouble(t);
+            double convD = (convVI + convVF) / 2 * convT;
+            return String.format("%.3f", convD);
+
+        } else if (t.equals("")) {
+            double convVF = Double.parseDouble(vf);
+            double convVI = Double.parseDouble(vi);
+            double convD = Double.parseDouble(d);
+            double convT = 2 * convD / (convVI + convVF);
+            return String.format("%.3f", convT);
+        } else {
+            return "Only one variable must be empty.";
+        }
     }
 }
