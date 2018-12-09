bd5f93a8e756626dd06358ec63b2564869c34f91
Joshua <jshin88@illinois.edu>
2018-12-08 19:53:55 -0600

    graph2 created

--- app/src/main/java/com/example/darkt/calculatorcs125/MainActivity.java
+++ app/src/main/java/com/example/darkt/calculatorcs125/MainActivity.java
@@ -52,20 +52,4 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-    public void buttonOnClick(View v) {
-        Button calculate = (Button) v;
-        EditText input = (EditText)findViewById(R.id.inputBox1);
-        String calculated = gForce(input.getText().toString());
-        TextView output = (TextView) findViewById(R.id.outputBox1);
-        output.setText(calculated);
-
-        Toast toast = new Toast(getApplicationContext());
-        toast.setGravity(Gravity.TOP | Gravity.LEFT, 0 , 0);
-        toast.makeText(MainActivity.this, "calculated", toast.LENGTH_SHORT).show();
-    }
-    public String gForce(String mass) {
-        double convMass = Double.parseDouble(mass);
-        double result = convMass * 9.8;
-        return String.format("%.3f", result);
-    }
-}
+ }
